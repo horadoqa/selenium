27ad4e01ce2e51728e3e01c264ef61f8fc5c6703
Ricardo Fahham <rfahham@hotmail.com>
2024-09-13 15:03:12 -0300

    Atualizando documentação

---
+++ projetos/youtube/youtube.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get('https://youtube.com')
+
+# Encontre o elemento que deseja clicar
+elemento = driver.find_element(By.XPATH, "//*[contains(text(), 'Informações Financeiras')]")
+elemento.click
+time.sleep(5)
+
+elemento2 = driver.find_element(By.XPATH, "//*[contains(text(), 'Planilha de Resultado')]")
+elemento2.click
+time.sleep(5)
+
+elemento3 = driver.find_element(By.XPATH, "//*[contains(text(), 'Planilha de Resultados Trimestrais')]")
+elemento3.click()
+time.sleep(5)
+
+# Feche o navegador
+driver.quit()
